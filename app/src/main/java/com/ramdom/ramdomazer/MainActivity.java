4d876b4fb5f9efa15330588bad01a099da49207b
denis <025896321Denis>
2020-02-21 12:44:43 +0200

    Initial commit

---
+++ app/src/main/java/com/ramdom/ramdomazer/MainActivity.java
@@ -0,0 +1,42 @@
+package com.ramdom.ramdomazer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    EditText maxRandomNumber;
+    TextView ramdomTextView;
+    Button start;
+    private Random random = new Random();
+    private String maxRandomNumberText;
+    private int randomNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        maxRandomNumber = findViewById(R.id.max_random_number_editText);
+        ramdomTextView = findViewById(R.id.ramdom_textView);
+        start = findViewById(R.id.start_button);
+
+        maxRandomNumber.requestFocus();
+
+        start.setOnClickListener(v ->{
+            maxRandomNumberText = maxRandomNumber.getText().toString();
+            if (!maxRandomNumberText.isEmpty()) {
+                randomNumber = random.nextInt(Integer.parseInt(maxRandomNumberText));
+                ramdomTextView.setText(Integer.toString(randomNumber));
+            }
+
+
+        });
+
+
+    }
+}
